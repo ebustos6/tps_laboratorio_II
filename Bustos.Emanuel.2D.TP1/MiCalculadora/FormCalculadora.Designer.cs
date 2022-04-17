b86c79ede1a11291f49bc57535de4e5fa57957e7
ebustos6 <ema.gnr6@gmail.com>
2022-04-17 09:38:08 -0300

    Comenzando el Win Form

--- Bustos.Emanuel.2D.TP1/MiCalculadora/FormCalculadora.Designer.cs
+++ Bustos.Emanuel.2D.TP1/MiCalculadora/FormCalculadora.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace MiCalculadora
 {
-    partial class Form1
+    partial class FormCalculadora
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // FormCalculadora
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Name = "FormCalculadora";
+            this.Text = "Calculadora de Emanuel Bustos del curso 2°D";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- Bustos.Emanuel.2D.TP1/MiCalculadora/Program.cs
+++ Bustos.Emanuel.2D.TP1/MiCalculadora/Program.cs
@@ -17,7 +17,7 @@ namespace MiCalculadora
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FormCalculadora());
         }
     }
 }
