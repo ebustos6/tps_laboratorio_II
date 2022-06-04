cab591082437ad36d3749158f67b9a6062fd3257
ebustos6 <ema.gnr6@gmail.com>
2022-06-04 11:00:46 -0300

    se agrego una carpeta para guardar los comprobantes en el escritorio

--- Bustos.Emanuel.2D.TP3/Entidades/Certificado.cs
+++ Bustos.Emanuel.2D.TP3/Entidades/Certificado.cs
@@ -14,18 +14,32 @@ namespace Entidades
         static Certificado()
         {
             ruta = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            ruta += @"/Certificados de Atencion";
         }
 
         public static void Escribir(Paciente paciente, Medico medico, DateTime fecha, string horario)
         {
             string nombreArchivo = ruta + @"/Certificado_" + paciente.NroObraSocial.ToString() + $"_{fecha.Day}_{fecha.Month}_{fecha.Year}.txt";
 
-            using (StreamWriter writer = new StreamWriter(nombreArchivo))
+            try
             {
-                writer.WriteLine($"El paciente {paciente.Apellido}, {paciente.Nombre} cuyo nro de obra social es: {paciente.NroObraSocial}");
-                writer.WriteLine($"fue atendido en esta institucion por el {medico.Nombre}, el dia {fecha.Day}/{fecha.Month}/{fecha.Year} a las {horario}hs.");
-                writer.WriteLine("\n\n\n\n\nSaluda Atte.\nFirma del profesional.");
+                if (!Directory.Exists(ruta))
+                {
+                    Directory.CreateDirectory(ruta);
+                }
+
+                using (StreamWriter writer = new StreamWriter(nombreArchivo))
+                {
+                    writer.WriteLine($"El paciente {paciente.Apellido}, {paciente.Nombre} cuyo nro de obra social es: {paciente.NroObraSocial}");
+                    writer.WriteLine($"fue atendido en esta institucion por el {medico.Nombre}, el dia {fecha.Day}/{fecha.Month}/{fecha.Year} a las {horario}hs.");
+                    writer.WriteLine("\n\n\n\n\nSaluda Atte.\nFirma del profesional.");
+                }
             }
+            catch (Exception)
+            {
+                throw new Exception($"Error en el archivo {nombreArchivo}, metodo Escribir()");
+            }
+            
             
         }
 
